f5ae284ab3e0e99147b416a49356e3b6ac03aad0
YiQianRong <Rong.YiQian512@Gmail.com>
2017-12-18 20:30:00 -0500

    Add files via upload

---
+++ PrimeSolution.Test/PrimeProcessorTest.cs
@@ -0,0 +1,160 @@
+﻿using NUnit.Framework;
+
+namespace PrimeSolution.Test
+{
+    [TestFixture]
+    public class PrimeProcessorTest
+    {
+        [Test]
+        public void PrimeTripletsTest()
+        {
+            //Problem 196 Prime triplets
+            //Build a triangle from all positive integers in the following way:
+            // 1
+            // 2-  3-
+            // 4   5-  6
+            // 7-  8   9  10
+            //11- 12  13- 14  15
+            //16  17- 18  19- 20  21
+            //22  23- 24  25  26  27  28
+            //29- 30  31- 32  33  34  35  36
+            //37- 38  39  40  41- 42  43- 44  45
+            //46  47- 48  49  50  51  52  53- 54 55
+            //56  57  58  59- 60  61- 62  63  64 65 66
+
+            //Each positive integer has up to eight neighbours in the triangle.
+            //A set of three primes is called a prime triplet if one of the three primes has the other two as neighbours in the triangle.
+            //For example, in the second row, the prime numbers 2 and 3 are elements of some prime triplet.
+            //If row 8 is considered, it contains two primes which are elements of some prime triplet, i.e. 29 and 31.
+            //If row 9 is considered, it contains only one prime which is an element of some prime triplet: 37.
+            //Define S(n) as the sum of the primes in row n which are elements of any prime triplet.
+            //Then S(8)=60 and S(9)=37.
+            //You are given that S(10000)=950007619.
+            //Find  S(5678027) + S(7208785).
+
+            //It is np problem
+
+            var primeList = PrimeProcessor.PrimeTriplets(1);
+            Assert.AreEqual(primeList[1], 1);
+
+            primeList = PrimeProcessor.PrimeTriplets(2);
+            Assert.AreEqual(primeList[1], 2);
+            Assert.AreEqual(primeList[2], 3);
+
+            primeList = PrimeProcessor.PrimeTriplets(3);
+            Assert.AreEqual(primeList[1], 5);
+
+            primeList = PrimeProcessor.PrimeTriplets(4);
+            Assert.AreEqual(primeList[1], 7);
+
+            primeList = PrimeProcessor.PrimeTriplets(5);
+            Assert.AreEqual(primeList[1], 11);
+            Assert.AreEqual(primeList[2], 13);
+
+            primeList = PrimeProcessor.PrimeTriplets(6);
+            Assert.AreEqual(primeList[1], 17);
+            Assert.AreEqual(primeList[2], 19);
+
+            primeList = PrimeProcessor.PrimeTriplets(7);
+            Assert.AreEqual(primeList[1], 23);
+
+            primeList = PrimeProcessor.PrimeTriplets(8);
+            Assert.AreEqual(primeList[1], 29);
+            Assert.AreEqual(primeList[2], 31);
+
+            primeList = PrimeProcessor.PrimeTriplets(9);
+            Assert.AreEqual(primeList[1], 37);
+            Assert.AreEqual(primeList[2], 41);
+            Assert.AreEqual(primeList[3], 43);
+
+            primeList = PrimeProcessor.PrimeTriplets(10);
+            Assert.AreEqual(primeList[1], 47);
+            Assert.AreEqual(primeList[2], 53);
+
+            primeList = PrimeProcessor.PrimeTriplets(11);
+            Assert.AreEqual(primeList[1], 59);
+            Assert.AreEqual(primeList[2], 61);
+
+            //To be completed
+        }
+
+        [Test]
+        public void CircularPrimesTest()
+        {
+            //Problem 35 Circular primes
+            //The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+            //There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+            //How many circular primes are there below one million?
+
+            //O(n * Log(n))
+
+            long primeProduct = 100000; //
+            var primeList = PrimeProcessor.CircularPrimes(primeProduct);
+            long result = primeList.Count;
+
+            Assert.AreEqual(result, 32);
+        }
+
+        [Test]
+        public void TheSumOfPrimesTest()
+        {
+            //Problem 10 Summation of primes
+            //The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+            //Find the sum of all the primes below two million.
+
+            //O(n * Log(n))
+
+            long primeProduct = 2000; //277050
+            var primeList = PrimeProcessor.PrimeList(primeProduct);
+            long result = 0;
+
+            foreach(long prime in primeList)
+            {
+                result += prime;
+            }
+
+            Assert.AreEqual(result, 277050);
+        }
+
+        [Test]
+        public void The_nth_PrimeTest()
+        {
+            //Problem 7 10001st prime projecteuler.net/problem=7
+            //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+            //What is the 10 001st prime number ?
+
+            //It is NP problem
+
+            long nth_prime = 10001; //104743
+            var largestPrimeFactors = PrimeProcessor.The_nth_Prime(nth_prime);
+            var result = largestPrimeFactors[largestPrimeFactors.Count-1];
+
+
+            Assert.AreEqual(nth_prime, largestPrimeFactors.Count);
+        }
+
+        [Test]
+        public void LargestPrimeFactorsTest()
+        {
+            //Problem 3 Largest prime factor projecteuler.net/problem=3
+            //The prime factors of 13195 are 5, 7, 13 and 29.
+            //What is the largest prime factor of the number 600851475143 ?
+
+            //O(n * Log(n))
+
+            //long printProduct = 13195; //5, 7, 13, 29
+            long primeProduct = 600851475143; //71, 839, 1471, 6857
+            var largestPrimeFactors = PrimeProcessor.LargestPrimeFactor(primeProduct);
+
+            long result = 1;
+            foreach (long prime in largestPrimeFactors)
+            {
+                result = result * prime;
+            }
+
+            Assert.AreEqual(primeProduct, result);
+        }
+
+
+    }
+}
---
+++ PrimeSolution/PrimeProcessor.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeSolution
+{
+    public class PrimeProcessor
+    {
+        //Problem 35196 Prime Triplets
+        //NP problem
+        public static List<long> PrimeTriplets(long n)
+        {
+            var primeTriplets = new Dictionary<long, List<long>>();
+            List<long> primes = new List<long>();
+            primes.Add(2);
+            primes.Add(3);
+            primes.Add(5);
+
+            long lastNumber = 1;
+            var rowList = new List<long>();
+            long len = primeTriplets.Count + 1;
+
+            for (long i = 1; primeTriplets.Count < n; i++)
+            {
+                if (rowList.Count == 0)
+                {
+                    rowList.Add(lastNumber);
+                }
+
+                bool isPrime = true;
+                foreach (long prime in primes)
+                {
+                    if (i <= prime) continue;
+
+                    isPrime = isPrime && (i % prime) != 0;
+                }
+
+                if (isPrime)
+                {
+                    if (!primes.Contains(i) && i > 1)
+                    {
+                        primes.Add(i);
+                    }
+
+                    if (i <= lastNumber)
+                    {
+                        rowList.Add(i);
+                    }else
+                    {
+                        primeTriplets.Add(len, rowList);
+                        len++;
+                        lastNumber = rowList[0] + len;
+                        rowList = new List<long>();
+                        rowList.Add(lastNumber);
+                        rowList.Add(i);
+                    }
+                }
+
+            }
+
+            var nList = primeTriplets[n];
+            return nList;
+        }
+
+        //Problem 35 Circular primes
+        //O(n * Log(n))
+        public static List<long> CircularPrimes(long n)
+        {
+            var circularPrimeList = new List<long>();
+            var primes = PrimeList(n);
+
+            circularPrimeList.Add(2);
+            circularPrimeList.Add(3);
+            circularPrimeList.Add(5);
+            circularPrimeList.Add(7);
+            circularPrimeList.Add(11);
+            circularPrimeList.Add(13);
+            circularPrimeList.Add(17);
+            circularPrimeList.Add(31);
+            circularPrimeList.Add(37);
+            circularPrimeList.Add(71);
+            circularPrimeList.Add(73);
+            circularPrimeList.Add(79);
+            circularPrimeList.Add(97);
+
+            foreach(var prime in primes)
+            {
+                if (prime < 100)
+                    continue;
+
+                string strNumber = prime.ToString();
+                List<long> numberList = All_Rotations(strNumber);
+
+                bool isContain = true;
+                foreach(long rotation in numberList)
+                {
+                    isContain = isContain && primes.Contains(rotation);
+                }
+
+                if(isContain)
+                {
+                    foreach (var number in numberList)
+                    {
+                        if (!circularPrimeList.Contains(number))
+                        {
+                            circularPrimeList.Add(number);
+                        }
+                    }
+                }
+            }
+
+            return circularPrimeList.OrderBy(p => p).ToList();
+        }
+
+        public static List<long> All_Rotations(string strNumber)
+        {
+            List<long> numbers = new List<long>();
+            numbers.Add(long.Parse(strNumber));
+            int firstLoop = strNumber.Length;
+            for(int i = 0; i< firstLoop; i++)
+            {
+                for (int j = i+1; j< firstLoop; j++)
+                {
+                    StringBuilder rotation = new StringBuilder(strNumber);
+                    char firstByte = rotation[i];
+                    rotation[i] = rotation[j];
+                    rotation[j] = firstByte;
+                    numbers.Add(long.Parse(rotation.ToString()));
+                }
+
+            }
+            return numbers;
+        }
+
+        //Problem 3 Largest prime factor projecteuler.net/problem=3
+        //O(n * Log(n))
+        public static List<long> LargestPrimeFactor(long n)
+        {
+            var largestPrimeFactor = new List<long>();
+            long printProduct = n;
+            bool isPrime = true;
+
+            List<long> primes = new List<long>();
+
+            for (long i = 2; i <= n; i++)
+            {
+                isPrime = true;
+                foreach (long prime in primes)
+                {
+                    isPrime = isPrime && (i % prime) != 0;
+                }
+
+                if (isPrime)
+                {
+                    long prime = i;
+                    primes.Add(prime);
+                    bool isPrimeFactor = (printProduct % prime) == 0;
+
+                    if (isPrimeFactor && printProduct > 1)
+                    {
+                        printProduct = printProduct / prime;
+
+                        largestPrimeFactor.Add(prime);
+                        n = printProduct;
+
+                        if (printProduct < i)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return largestPrimeFactor;
+        }
+
+        //O(n * Log(n))
+        public static List<long> PrimeList(long n)
+        {
+            List<long> primes = new List<long>();
+            primes.Add(2);
+            primes.Add(3);
+            primes.Add(5);
+            primes.Add(7);
+            primes.Add(11);
+            primes.Add(13);
+            primes.Add(17);
+            primes.Add(19);
+            primes.Add(23);
+            primes.Add(29);
+            primes.Add(31);
+            primes.Add(37);
+            primes.Add(41);
+            primes.Add(43);
+            primes.Add(47);
+            primes.Add(53);
+            primes.Add(59);
+            primes.Add(61);
+
+            for (long i = 62; i < n; i++)
+            {
+                bool isPrime = true;
+                foreach (long prime in primes)
+                {
+                    isPrime = isPrime && (i % prime) != 0;
+                }
+
+                if (isPrime)
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+        }
+
+        //It is NP problem
+        public static List<long> The_nth_Prime(long n)
+        {
+            List<long> primes = new List<long>();
+            primes.Add(2);
+            primes.Add(3);
+            primes.Add(5);
+            primes.Add(7);
+            primes.Add(11);
+            primes.Add(13);
+            primes.Add(17);
+            primes.Add(19);
+            primes.Add(23);
+            primes.Add(29);
+            primes.Add(31);
+            primes.Add(37);
+            primes.Add(41);
+            primes.Add(43);
+            primes.Add(47);
+            primes.Add(53);
+            primes.Add(59);
+            primes.Add(61);
+
+            for (long i = 62; primes.LongCount() < n; i++)
+            {
+                bool isPrime = true;
+                foreach (long prime in primes)
+                {
+                    isPrime = isPrime && (i % prime) != 0;
+                }
+
+                if (isPrime)
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+        }
+    }
+}
