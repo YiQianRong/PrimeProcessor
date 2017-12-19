25f236046a043d47eb3a4346ecfd5783028a67da
YiQianRong <Rong.YiQian512@Gmail.com>
2017-12-18 21:45:34 -0500

    Add files via upload

--- PrimeSolution.Test/PrimeProcessorTest.cs
+++ PrimeSolution.Test/PrimeProcessorTest.cs
@@ -1,10 +1,35 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace PrimeSolution.Test
 {
     [TestFixture]
     public class PrimeProcessorTest
     {
+        //[Test]
+        public void ChineseLeftoversIITest()
+        {
+            //Problem 552 Chinese leftovers II
+            //Let An be the smallest positive integer satisfying An mod pi = i for all 1 ≤ i ≤ n, where pi is the i-th prime. 
+            //For example A2 = 5, since this is the smallest positive solution of the system of equations 
+            // A2 mod 2 = 1 
+            // A2 mod 3 = 2
+            //
+            //The system of equations for A3 adds another constraint. That is, A3 is the smallest positive solution of
+            // A3 mod 2 = 1 
+            // A3 mod 3 = 2
+            // A3 mod 5 = 3
+            //
+            //and hence A3 = 23. Similarly, one gets A4 = 53 and A5 = 1523. 
+            //Let S(n) be the sum of all primes up to n that divide at least one element in the sequence A. 
+            //For example, S(50) = 69 = 5 + 23 + 41, since 5 divides A2, 23 divides A3 and 41 divides A10 = 5765999453. No other prime number up to 
+            //50 divides an element in A. 
+            //
+            //Find S(300000).
+        }
+
         [Test]
         public void PrimeTripletsTest()
         {
@@ -90,9 +115,9 @@ namespace PrimeSolution.Test
 
             long primeProduct = 100000; //
             var primeList = PrimeProcessor.CircularPrimes(primeProduct);
-            long result = primeList.Count;
+            long result = primeList.OrderByDescending(p => p).ToList().FirstOrDefault();
 
-            Assert.AreEqual(result, 32);
+            Assert.AreEqual(result, 9311);
         }
 
         [Test]
@@ -123,14 +148,13 @@ namespace PrimeSolution.Test
             //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
             //What is the 10 001st prime number ?
 
-            //It is NP problem
+            //It is a NP problem
 
             long nth_prime = 10001; //104743
             var largestPrimeFactors = PrimeProcessor.The_nth_Prime(nth_prime);
             var result = largestPrimeFactors[largestPrimeFactors.Count-1];
 
-
-            Assert.AreEqual(nth_prime, largestPrimeFactors.Count);
+            Assert.AreEqual(result, 104743);
         }
 
         [Test]
