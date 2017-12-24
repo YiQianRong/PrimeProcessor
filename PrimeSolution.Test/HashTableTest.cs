95d64375d1eaca8b2289f875b38fcf159dc955a5
YiQianRong <Rong.YiQian512@Gmail.com>
2017-12-24 15:57:45 -0500

    Add files via upload

---
+++ PrimeSolution.Test/HashTableTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace HashTable.Test
+{
+    class HashTableTest
+    {
+        [Test]
+        public void HasTableTypesTest()
+        {
+            var hashTable = new HashTable<int, string>();
+            hashTable[1] = "Test1";
+            hashTable[100] = "Test2";
+            Assert.AreEqual(hashTable[1], "Test1");
+            Assert.AreEqual(hashTable[100], "Test2");
+            Assert.AreEqual(hashTable.Count, 2);
+            hashTable.Remove(100);
+            Assert.AreEqual(hashTable.Count, 1);
+            hashTable.Clear();
+            Assert.AreEqual(hashTable.Count, 0);
+        }
+
+        [Test]
+        public void HasTableOjectTypeTest()
+        {
+            var hashTable = new HashTable();
+            hashTable[2] = "Test1";
+            hashTable[200] = "Test2";
+            Assert.AreEqual(hashTable[2], "Test1");
+            Assert.AreEqual(hashTable[200], "Test2");
+            hashTable.Remove(200);
+            Assert.AreEqual(hashTable.Count, 1);
+            hashTable.Clear();
+            Assert.AreEqual(hashTable.Count, 0);
+        }
+    }
+}
---
+++ PrimeSolution.Test/StackTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System.Text;
+
+namespace Stack.Test
+{
+    [TestFixture]
+    public class StackTest
+    {
+        [Test]
+        public void StackIsEmptyTest()
+        {
+            var intStack = new Stack<int>();
+            bool result = true;
+
+            Assert.AreEqual(result, intStack.IsEmpty);
+        }
+
+        [Test]
+        public void StackPushPopTest()
+        {
+            var charStack = new Stack<char>();
+            charStack.Push('A');
+            charStack.Push('B');
+            charStack.Push('C');
+            charStack.Push('D');
+            charStack.Push('E');
+
+            var newString = new StringBuilder();
+            while (!charStack.IsEmpty)
+            {
+                var node = charStack.Pop();
+                newString.Append(node.Value);
+            }
+
+            string result = "EDCBA";
+            Assert.AreEqual(result, newString.ToString());
+        }
+
+        [Test]
+        public void StackSizeTest()
+        {
+            var charStack = new Stack<char>();
+            charStack.Push('A');
+            charStack.Push('B');
+            charStack.Push('C');
+            charStack.Push('D');
+            charStack.Push('E');
+            charStack.Push('1');
+            var node = charStack.Pop();
+
+            int result = 5;
+            Assert.AreEqual(result, charStack.Size);
+        }
+    }
+}
